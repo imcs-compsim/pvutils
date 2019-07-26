3d8174230fb09e764cb36ce69600048326be9e5e
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2019-07-26 17:17:25 +0200

    Add test for full postprocessing script #1

--- tests/test_local.sh
+++ tests/test_local.sh
@@ -10,7 +10,6 @@ ORIGINAL_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 echo "Test pvutils with pvpython"
 export PYTHONPATH="${ORIGINAL_PYTHONPATH}"
 export LD_LIBRARY_PATH="${ORIGINAL_LD_LIBRARY_PATH}"
-export IS_PVPYTHON=1
 ${PARAVIEW_PATH}/bin/pvpython testing_pvutils.py
 
 # Test with system python.
@@ -18,7 +17,6 @@ echo ""
 echo "Test pvutils with system python"
 export PYTHONPATH="${ORIGINAL_PYTHONPATH}:${PARAVIEW_PATH}/lib/python2.7/site-packages"
 export LD_LIBRARY_PATH="${ORIGINAL_LD_LIBRARY_PATH}:${PARAVIEW_PATH}/lib"
-export IS_PVPYTHON=1
 python2 testing_pvutils.py
 
 # Test with paraview. This will open a GUI that has to be closed by the user.
@@ -26,5 +24,4 @@ echo ""
 echo "Test pvutils with paraview (will open GUI)"
 export PYTHONPATH="${ORIGINAL_PYTHONPATH}"
 export LD_LIBRARY_PATH="${ORIGINAL_LD_LIBRARY_PATH}"
-export IS_PVPYTHON=0
 ${PARAVIEW_PATH}/bin/paraview --script=testing_pvutils.py
