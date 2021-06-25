723030f37ad6120d83516cd47171f1b83c4dfc35
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2021-06-25 08:34:44 +0200

    Cleanup formating of imports

--- pvutils/__init__.py
+++ pvutils/__init__.py
@@ -5,20 +5,50 @@ module.
 """
 
 # Load utility functions.
-from .utility_functions import (load_file, display, contour, check_data,
-    get_base, reset_paraview, programmable_filter, setup_view,
-    print_view_state, get_size_pixel, is_pvpython, set_colorbar_font,
-    get_available_timesteps, set_timestep, get_field_names, get_view,
-    get_display, get_parents, get_source_name, print_view_state_set_scalar_bar,
-    update_scene, set_color_range, programmable_source, add_coordinate_axes,
-    get_vtk_data_as_numpy, get_bounding_box)
+from .utility_functions import (
+    add_coordinate_axes,
+    check_data,
+    contour,
+    display,
+    get_available_timesteps,
+    get_base,
+    get_bounding_box,
+    get_display,
+    get_field_names,
+    get_parents,
+    get_size_pixel,
+    get_source_name,
+    get_view,
+    get_vtk_data_as_numpy,
+    is_pvpython,
+    load_file,
+    print_view_state,
+    print_view_state_set_scalar_bar,
+    programmable_filter,
+    programmable_source,
+    reset_paraview,
+    set_color_range,
+    set_colorbar_font,
+    set_timestep,
+    setup_view,
+    update_scene
+    )
 
-# Load the wrappers.
-from .filter_wrapper import (glyph, warp, transform, threshold, tube,
-    temporal_interpolator, clip)
+# Load the filter wrappers.
+from .filter_wrapper import (
+    clip,
+    glyph,
+    temporal_interpolator,
+    threshold,
+    transform,
+    tube,
+    warp
+    )
 
 # Load utility classes.
-from .utility_classes import (BeamDisplay)
+from .utility_classes import (
+    BeamDisplay
+    )
 
 # Load scripts.
 import scripts
